13ef0ac22c189561ca01c8d413342483d117925d
Victor Costa <dxhj@hotmail.com.br>
2015-04-23 11:59:56 -0300

    Create dfa.py

---
+++ lexical_analysis/examples/dfa.py
@@ -0,0 +1,21 @@
+# construct an NFA and by subset construction an equivalent DFA, simulate by this approach:
+
+def simulate(input, dfa, start, accepting):
+  s = start
+  for c in input:
+    try:
+      s = dfa[s,c]
+    except KeyError:
+    	return False
+  if s in accepting:
+    return True
+  return False
+
+# Dictionary in the form (state, input_symbol) = next_state
+example = {(0, "A"): 0, (0, "B"): 1}
+
+# A list of accepting states
+F = [1]
+
+simulate("AAAB", example, 0, F) # True
+simulate("AAAABx", example, 0, F) # False
